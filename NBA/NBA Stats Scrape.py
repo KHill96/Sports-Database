81dac3f48f80a04f5a5b39a957d00a821cc58de8
Kenny <KHill0996@gmail.com>
2019-11-13 18:32:39 -0500

    Got rid of a warning message

--- NBA/NBA Stats Scrape.py
+++ NBA/NBA Stats Scrape.py
@@ -3,33 +3,36 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import pandas
 
-# Links to the types of stats I want to scrape
-links = ['https://www.basketball-reference.com/leagues/NBA_{}_totals.html',
-        'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
-        ,'https://www.basketball-reference.com/leagues/NBA_{}_per_minute.html'
-        ,'https://www.basketball-reference.com/leagues/NBA_{}_per_poss.html']
+def main():
+    # Links to the types of stats I want to scrape
+    links = ['https://www.basketball-reference.com/leagues/NBA_{}_totals.html',
+            'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
+            ,'https://www.basketball-reference.com/leagues/NBA_{}_per_minute.html'
+            ,'https://www.basketball-reference.com/leagues/NBA_{}_per_poss.html']
 
-# Create a list of years from 1974 to 2019 since the 2019-2020 only just got started a few weeks ago
-yearList = []
-yearList.extend(range(1974,2020))
-for year in yearList:
-    for link in links:
-        #Replace the {} characters with the year
-        link = link.format(year)
+    # Create a list of years from 1974 to 2019 since the 2019-2020 only just got started a few weeks ago
+    yearList = []
+    yearList.extend(range(1974,2020))
+    for year in yearList:
+        for link in links:
+            #Replace the {} characters with the year
+            link = link.format(year)
 
-        # Get the html to scrape
-        html = urlopen(link)
-        soup = bs(html)
+            # Get the html to scrape
+            html = urlopen(link)
+            soup = bs(html,'html.parser')
 
-        #Name the columns of the csv
-        headerRow = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-        headerRow = headerRow[1:]
-        # Get the data using findAll
-        rows = soup.findAll('tr')[1:]
-        stats = [[td.getText() for td in rows[i].findAll('td')]for i in range(len(rows))]
-        stats = pandas.DataFrame(stats,columns= headerRow)
-        fileName = link[link.rfind('NBA'):].replace('_',' ')
-        # Cleanup the file name and create the csv
-        fileName = fileName.replace('.html','')
-        fileName = fileName +'.csv'
-        stats.to_csv(fileName,index=False)
+            #Name the columns of the csv
+            headerRow = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+            headerRow = headerRow[1:]
+            # Get the data using findAll
+            rows = soup.findAll('tr')[1:]
+            stats = [[td.getText() for td in rows[i].findAll('td')]for i in range(len(rows))]
+            stats = pandas.DataFrame(stats,columns= headerRow)
+            fileName = link[link.rfind('NBA'):].replace('_',' ')
+            # Cleanup the file name and create the csv
+            fileName = fileName.replace('.html','')
+            fileName = fileName +'.csv'
+            stats.to_csv(fileName,index=False)
+
+main()
