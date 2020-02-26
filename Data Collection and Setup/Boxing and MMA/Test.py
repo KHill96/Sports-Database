1a23a7bdc9bff17a571b903a98cb37680da128e3
KHill96 <KHill0996@gmail.com>
2020-02-26 16:59:39 -0500

    Toying with a way to scrape fighter data using a data structure/graph

--- Data Collection and Setup/Boxing and MMA/Test.py
+++ Data Collection and Setup/Boxing and MMA/Test.py
@@ -1,62 +1,80 @@
-# TODO: Trim the list
 # TODO: Try to build the dataframe with Khabib and Floyd. See if there isn't a way to have just one data frame
-# TEST ON A DUMMY LAPTOP (Macbook, forget the speed)
-# IDEA: 6 degrees of *Insert Name*
-#ISSUE: Guys like James Toney and Conor McGregor did both Boxing and MMA.
+# TODO: split up the crossover fighters. Boxing to MMA has the boxing record come up first, MMA to boxing is the other way around
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import json
+import pandas
 
 # Because wikipedia sucks I have to make two data frames:
     # One for MMA Fighters
     # One for Boxers
+# There are some fighters who competed in both sports professionally but wikipedia will put their "main" sport as the first table
+# So James Toney has his boxing record come up first instead of his MMA record
 
-# Build the set of links
-def buildBoxerSet():
-    with open('Boxers.json') as f:
-        data = json.load(f)
-   
-    # Create a set to hold links that we already scraped
-    boxerSet = set()
-    for link in data["boxerLinks"]:
-        boxerSet.add(link)
-    boxerSet_Crawled = set()
-    # while the set doesn't have all the links from the JSON
-    for link in boxerSet:
-        # If the link is in the set pass to the next
-        if link not in boxerSet_Crawled:
-            html = urlopen(link)
-            soup = bs(html,'html.parser')
-            table = soup.findAll('table',{'class':'wikitable'})[1]
-            rows = table.findAll('tr')
-            tds = [a for a in rows.findAll('td').find('a')]
-            print(td)
-        # Else Scrape it for the thinks
-    # Export the final file(s)
+# Global
+boxerSet = set()
+mmaSet = set()
+crossoverSet = set()
+df = pandas.DataFrame()
+wikiTemplate = 'https://en.wikipedia.org{}'
 
-# MMA Fighter Scrape
-def buildMMAFighterSet():
-    # Beautiful Soup Drill
-    wikiTemplate = 'https://en.wikipedia.org{}'
-    with open('MMA_Fighters.json') as f:
-        data = json.load(f)
-    mmaSet = set()
-    for link in data['mmaFighterLinks']:
+def buildMMASet():
+    # MMA Set
+    with open('MMA_Fighters.json') as MMA_JSON:
+        data = json.load(MMA_JSON)
+    for link in data["mmaFighterLinks"]:
         mmaSet.add(link)
-    html = urlopen(url)
-    soup = bs(html,'html.parser')
-    table = soup.findAll('table',{'class':'wikitable'})[1]
-    rows = table.findAll('tr')
-    tds = [row.findAll('td') for row in rows]
-    # Change the row not the column
-    test = []
-    for i in range(1,len(rows)):
-        test.append(tds[i][2].find('a',href=True))
-    print(wikiTemplate.format(test[0]['href']))
+
+    # Crawl links
+    # print(mmaSet.__contains__('https://en.wikipedia.org/wiki/Khabib_Nurmagomedov'))
+    seen = set()
+    # Playing dangerously
+    while seen != mmaSet:
+        try:
+            for link in mmaSet:
+                if seen.__contains__(link):
+                    mmaSet.add(link)
+                    pass
+                else:
+                    seen.add(link)
+                    print(link)
+                    html = urlopen(link)
+                    soup = bs(html,'html.parser')
+                    recordTable = soup.findAll('table',{'class':'wikitable'})[1]
+                    rows = recordTable.findAll('tr')
+                    tds = [row.findAll('td') for row in rows]
+                    for i in range(1,len(rows)):
+                        try:
+                            linkRetrieved = tds[i][2].find('a',href=True)
+                            mmaSet.add(wikiTemplate.format(linkRetrieved['href']))
+                        except TypeError:
+                            pass
+                        except IndexError:
+                            continue
+        except RuntimeError:
+            continue
+    with open('test1.txt','w') as testFile:
+        for link in mmaSet:
+            testFile.write(link)
+            testFile.write('\n')
+        
     
-# def boxerScrape():
+def RecordScrapeMMA(link):
+
+    soup = bs(urlopen(link),'html.parser')
+    recordTable = soup.findAll('table',{'class':'wikitable'})[1]
+    rows = recordTable.findAll('th')
+    print(rows[1].text)
+# FOR BOXERS!!!!!!
+    # test = rows[1].findAll('td')[3].findAll('a')[1]
+
 
 # Main
 def main():
-    MMA_FighterScrape()
+    with open('CrossoverFighters.json') as crossoverJSON:
+        data = json.load(crossoverJSON)
+    for link in data["BoxingToMMA"]:
+        crossoverSet.add(link)
+    buildMMASet()
 main()
